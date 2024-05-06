a4132b19e3070bac7f8116db47e558da02123576
Shuang Wu <shuang111994@gmail.com>
2024-05-05 17:51:18 -0700

    Implemented basic player stateMachine

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public PlayerStateMachine stateMachine {  get; private set; }
+    [Header("Move info")]
+    public float moveSpeed = 12;
+    public float jumpForce;
+    #region Components
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    #endregion
 
+    #region States
+    public PlayerStateMachine stateMachine {  get; private set; }
     public PlayerIdleState idleState { get; private set; } 
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
+
+    #endregion
 
     private void Awake()
     {
@@ -16,16 +28,24 @@ public class Player : MonoBehaviour
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
-
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start()
     {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         stateMachine.Initialize(idleState);
     }
 
     private void Update()
     {
         stateMachine.currentState.Update();
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
     }
 }
--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -7,6 +7,9 @@ public class PlayerState
     protected PlayerStateMachine stateMachine;
     protected Player player;
 
+    protected Rigidbody2D rb;
+
+    protected float xInput;
     private string animBoolName;
 
    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
@@ -19,17 +22,20 @@ public class PlayerState
 
     public virtual void Enter()
     {
-
+        player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
     }
 
     
     public virtual void Update()
     {
+        xInput = Input.GetAxisRaw("Horizontal");
 
+        player.anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     public virtual void Exit()
     {
-
+        player.anim.SetBool(animBoolName, false);
     }
 }
